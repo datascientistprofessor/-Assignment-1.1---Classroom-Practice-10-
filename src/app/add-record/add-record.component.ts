345a8df3008f1f0f15d8b24cf240790f7fab9362
ne-plusultra <60425996+ne-plusultra@users.noreply.github.com>
2021-02-04 21:49:04 +0600

    master

---
+++ src/app/add-record/add-record.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-record',
+  templateUrl: './add-record.component.html',
+  styleUrls: ['./add-record.component.css']
+})
+export class AddRecordComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}
